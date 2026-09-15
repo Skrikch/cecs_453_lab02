b343d7e20e156a2c5f9194c7bfee3a561db9bef3
Christos G <xgeo2000@pm.me>
2026-09-15 00:00:16 -0700

    Added assets, Refactored all UI elements

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(GalleryApp());
 }
 
-class Artwork
-{
+// Artwork class to easily represent the data we want to display
+class Artwork {
   final String artist;
   final int year;
   final String imageAsset;
@@ -15,149 +14,163 @@ class Artwork
   const Artwork(this.artist, this.year, this.imageAsset, this.title);
 }
 
-final List<Artwork> _art_pieces =
-[
-  Artwork('Smunga', 2001, 'assets/fence_rat.jpg', 'SmoogTitle'),
-  Artwork('Gubby', 2020, 'assets/RatGamer.jpg', 'GoobTitle'),
-  Artwork('SmunGubba', 10101, 'assets/him.jpg', 'Both')
+// Storing artwork objects in a list to iterate through with buttons
+final List<Artwork> _art_pieces = [
+  Artwork('Unknown', 2021, 'assets/fence_rat.jpg', 'Rat Upon a Fence'),
+  Artwork('Unknown', 2023, 'assets/RatGamer.jpg', 'Gamer Rat'),
+  Artwork('Unknown', 2020, 'assets/him.jpg', 'Rat Upon a Couch'),
+  Artwork(
+    'Christos',
+    2026,
+    'assets/Smunga_Staring_at_Camera.jpeg',
+    'Mr. Smunga Eyeing You',
+  ),
+  Artwork('Christos', 2021, 'assets/Bella.jpg', 'Bella Looking Cute'),
+  Artwork('Britney', 2026, 'assets/Gub_in_box.jpeg', 'Mr. Gubbins in his box'),
+  Artwork(
+    'Unknown',
+    2019,
+    'assets/MJK.png',
+    "Maynard James Keenan Messily Eating a Donut",
+  ),
 ];
 
-class GalleryApp extends StatelessWidget
-{
+// Our primary class, stateless
+class GalleryApp extends StatelessWidget {
   const GalleryApp({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
-    return MaterialApp
-    (
-      title: 'B + C Gallery',
-      home: GalleryScreen(),
-    );
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'B + C Gallery', home: GalleryScreen());
   }
 }
 
-class GalleryScreen extends StatefulWidget
-{
+// Stateful widget representing the image and description
+class GalleryScreen extends StatefulWidget {
   @override
   _GalleryScreenState createState() => _GalleryScreenState();
 }
 
-class _GalleryScreenState extends State<GalleryScreen>
-{
-  // put variables here
-  int _currentAsset = 0;    // for keeping track of which image should show
+// State implementation
+class _GalleryScreenState extends State<GalleryScreen> {
+  int _currentAsset = 0; // for keeping track of which image should show
 
-  // put functions here
-  void _incrementGallery()
-  {
-    if (_currentAsset == _art_pieces.length - 1)
-    {
-      setState(() {_currentAsset = 0;});
-    }
-    else
-    {
-      setState(() {_currentAsset++;});
+  /// HELPER FUNCTIONS
+  void _incrementGallery() {
+    if (_currentAsset == _art_pieces.length - 1) {
+      setState(() {
+        _currentAsset = 0;
+      });
+    } else {
+      setState(() {
+        _currentAsset++;
+      });
     }
   }
 
-  void _decrementGallery()
-  {
-    if (_currentAsset == 0)
-    {
-      setState(() {_currentAsset = _art_pieces.length - 1;});
-    }
-    else
-    {
-      setState(() {_currentAsset--;});
+  void _decrementGallery() {
+    if (_currentAsset == 0) {
+      setState(() {
+        _currentAsset = _art_pieces.length - 1;
+      });
+    } else {
+      setState(() {
+        _currentAsset--;
+      });
     }
   }
 
+  // GUI design and implementation
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold
-    (
-      appBar: AppBar
-      (
-          title: const Text('B+C Gallery'),
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Britney and Christos\' Photo Gallery'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
       ),
 
-      body: Expanded
-      (
-        child: Column
-        (
-          children:
-          [
-            Padding
-            (
-              padding: EdgeInsets.fromLTRB(50, 50, 50, 50),
+      // Root of our scaffold, Centered Column
+      body: Center(
+        child: Column(
+          children: [
+            // Begin by padding all sides
+            Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+            // Next, display and expand the image
+            Expanded(
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  child: Image.asset(
+                    _art_pieces[_currentAsset].imageAsset,
+                    width: 300,
+                  ),
+                ),
+                elevation: 15,
+              ),
             ),
 
-            Expanded
-            (
-              child: Column
-              (
-                children:
-                [
-                  // image,
-                  Image.asset(_art_pieces[_currentAsset].imageAsset, width: 300),
+            // Padding between Image and Card
+            Padding(padding: EdgeInsets.symmetric(vertical: 10)),
 
-                  Padding
-                  (
-                    padding: EdgeInsets.symmetric(vertical: 8)
+            // Card for our image descriptions
+            Card.filled(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: SizedBox(
+                  // Finding width of screen to properly size title card
+                  width: MediaQuery.sizeOf(context).width - 80,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "${_art_pieces[_currentAsset].title}",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "\n${_art_pieces[_currentAsset].artist} (${_art_pieces[_currentAsset].year})",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
                   ),
-
-                  // text box,
-                  Column
-                  (
-                      children:
-                      [
-                        Text(_art_pieces[_currentAsset].title),
-                        Text("${_art_pieces[_currentAsset].artist} (${_art_pieces[_currentAsset].year})"),
-                      ]
-                  ),
-                ]
-              )
+                ),
+              ),
             ),
 
-            // buttons,
-            Row
-            (
+            // Padding between card and buttons
+            Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+
+            // Buttons, allow us to change the state of the above widgets
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children:
-              [
-                ElevatedButton
-                (
+              children: [
+                ElevatedButton(
                   onPressed: _decrementGallery,
                   child: const Text('<- Previous'),
-                  style: ElevatedButton.styleFrom
-                  (
-                      minimumSize: Size(125, 40),
-                      backgroundColor: Colors.blueGrey,
-                      foregroundColor: Colors.white,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(125, 40),
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
                   ),
                 ),
-                ElevatedButton
-                (
+                ElevatedButton(
                   onPressed: _incrementGallery,
                   child: const Text('Next ->'),
-                  style: ElevatedButton.styleFrom
-                  (
-                      minimumSize: Size(125, 40),
-                      backgroundColor: Colors.blueGrey,
-                      foregroundColor: Colors.white,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(125, 40),
+                    backgroundColor: Colors.blueGrey,
+                    foregroundColor: Colors.white,
                   ),
                 ),
-              ]
+              ],
             ),
 
-            Padding
-            (
-              padding: EdgeInsets.only(bottom: 80)
-            ),
+            // Bottom padding to keep us away from the home bar / gesture region
+            Padding(padding: EdgeInsets.only(bottom: 80)),
           ],
         ),
       ),
